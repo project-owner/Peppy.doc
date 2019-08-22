1f4eec7f5257aaa11b4550cada3e2a4d6e9028a3
project-owner <peppy.player@gmail.com>
2019-08-22 10:45:47 -0700

    Holbein Edition

--- files/installsdl.sh
+++ files/installsdl.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #enable wheezy package sources
-echo "deb http://archive.raspbian.org/raspbian wheezy main
+echo "deb http://legacy.raspbian.org/raspbian wheezy main
 " > /etc/apt/sources.list.d/wheezy.list
 
-#set stable as default package source (currently jessie)
-echo "APT::Default-release \"stretch\";
+#set stable as default package source (currently buster)
+echo "APT::Default-release \"buster\";
 " > /etc/apt/apt.conf.d/10defaultRelease
 
-#set the priority for libsdl from wheezy higher then the jessie package
+#set the priority for libsdl from wheezy higher then the buster package
 echo "Package: libsdl1.2debian
-Pin: release n=jessie
+Pin: release n=buster
 Pin-Priority: -10
 Package: libsdl1.2debian
 Pin: release n=wheezy
